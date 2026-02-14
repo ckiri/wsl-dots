03d666b3d6ba683e76b7f4d4851eaca29599f34f
Chris Kiriakou <chris-kiriakou@web.de>
2026-02-14 23:33:42 +0100

    Update setup script.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-# Setup development environment in WSL
+# Setup a WSL development environment
 
-sudo apt install -y \
+sep="================================================================================"
+
+sudo apt install \
 	git \
 	zsh \
 	lf \
@@ -25,29 +27,35 @@ test ! -d ~/.cache/zsh && mkdir -p ~/.cache/zsh
 test ! -f ~/.cache/zsh/history && touch ~/.cache/zsh/history
 
 # Install Neovim using bob
+printf "${sep}\nInstalling Neovim\n${sep}\n\n"
 curl -L -O 'https://github.com/MordechaiHadad/bob/releases/download/v4.1.6/bob-linux-x86_64.zip'
-unzip bob-linux-x86_64.zip
+test -f ./bob-linux-x86_64.zip && unzip ./bob-linux-x86_64.zip
 test ! -d ~/.local/bin && mkdir -p ~/.local/bin
 mv bob-linux-x86_64/bob ~/.local/bin/bob
 rm -r ./*.zip
 sudo chmod +x ~/.local/bin/bob
-bob install nightly
-bob use nightly
+~/.local/bin/bob install nightly
+~/.local/bin/bob use nightly
+printf "\n\n"
 
 # Generate a GPG key
-printf "Generating a GPG key\n"
+printf "${sep}\nGenerating a GPG key\n${sep}\n\n"
 gpg --full-gen-key
+printf "\n\n"
 
 # Init password safe
+printf "${sep}\nInitialize Pass\n${sep}\n\n"
 read email "Enter the same email used for generating the GPG key: "
 pass init $email
+printf "\n\n"
 
 # Setup Git
-printf "Setting up Git..."
+printf "${sep}\nSetting up Git\n${sep}\n\n"
 read email "Enter your email address: "
 git config --global user.email "${email}"
 read name "Enter your full name: " 
 git config --global user.name "${name}"
+printf "\n\n"
 
 # Change the login shell to Z-Shell
 sudo chsh -s $(which zsh)
